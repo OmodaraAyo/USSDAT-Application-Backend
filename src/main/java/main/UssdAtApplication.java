68d87ac39308f4fc714755b5ee9531967b1f5f3c
OmodaraAyo <ayodeleomodara1234@gmail.com>
2025-04-08 13:21:47 +0100

    first commit

---
+++ src/main/java/main/UssdAtApplication.java
@@ -0,0 +1,11 @@
+package main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UssdAtApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(UssdAtApplication.class, args);
+    }
+}
